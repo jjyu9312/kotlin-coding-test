c964c0af0ac72a9906e57403174fc5f38cf059d3
Kyungwook Kim <kwkim@autocrypt.io>
2023-08-10 00:45:22 +0900

    1085 commit

---
+++ src/main/kotlin/august/acmicpc1085.kt
@@ -0,0 +1,18 @@
+package august
+
+import java.util.*
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    val x = nextInt()
+    val y = nextInt()
+    val w = nextInt()
+    val h = nextInt()
+
+    val list = mutableListOf<Int>()
+    list.add(x - 0)
+    list.add(y - 0)
+    list.add(w - x)
+    list.add(h - y)
+
+    println(list.minByOrNull { it })
+}
