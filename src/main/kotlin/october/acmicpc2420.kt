c82392e99c876a1218de1c40142bf08e0f782d99
Kyungwook Kim <kwkim@autocrypt.io>
2023-10-20 11:20:13 +0900

    사파리 월드 abs 이용

--- src/main/kotlin/october/acmicpc2420.kt
+++ src/main/kotlin/october/acmicpc2420.kt
@@ -1,14 +1,13 @@
 package october
 
 import java.util.*
+import kotlin.math.abs
 
 fun main(args: Array<String>) = with(Scanner(System.`in`)) {
     val n = nextInt()
     val m = nextInt()
     val minusValue = n - m
-    val answer =
-        if (minusValue  < 0) - minusValue
-        else minusValue
+    val answer = abs(minusValue)
 
     println(answer)
 }
