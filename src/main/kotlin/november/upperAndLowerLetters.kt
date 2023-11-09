5832124d923f78b22ac5a75373af17831004a8ed
kevin <jjyu9312@gmail.com>
2023-11-09 15:21:19 +0900

    알파벳 대소문자 변환

---
+++ src/main/kotlin/november/upperAndLowerLetters.kt
@@ -0,0 +1,18 @@
+package november
+
+import java.util.*
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    val letters = next()
+    val charList = mutableListOf<Char>()
+    for ((index, item) in letters.withIndex()) {
+        if (item.isUpperCase()) {
+            charList.add(item.toLowerCase())
+        } else {
+            charList.add(item.toUpperCase())
+        }
+    }
+
+    val answer = charList.joinToString("")
+    println(answer)
+}
