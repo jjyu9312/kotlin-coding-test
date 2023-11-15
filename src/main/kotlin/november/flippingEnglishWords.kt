997678b041447c140a78a9b6d21259b6b3475784
kevin <jjyu9312@gmail.com>
2023-11-15 16:34:24 +0900

    알파벳만 앞 뒤로 바꾸기

--- src/main/kotlin/november/flippingEnglishWords.kt
+++ src/main/kotlin/november/flippingEnglishWords.kt
@@ -4,7 +4,23 @@ import java.util.*
 
 fun main(args: Array<String>) = with(Scanner(System.`in`)) {
     val words = next()
-    var answer = ""
 
+    val charList: MutableList<Char> = words.toMutableList()
+    var lt = 0
+    var rt = words.length - 1
+
+    while (lt < rt) {
+        if (!charList[lt].isLetter()) lt ++
+        else if (!charList[rt].isLetter()) rt --
+        else {
+            val tmp = charList[lt]
+            charList[lt] = charList[rt]
+            charList[rt] = tmp
+            lt ++
+            rt --
+        }
+    }
+
+    val answer = charList.joinToString("")
     println(answer)
 }
