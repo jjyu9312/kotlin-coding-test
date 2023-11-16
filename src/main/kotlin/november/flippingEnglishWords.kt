0073444c7b00a27dccabda02c9bc6cd60c67e45a
kevin <jjyu9312@gmail.com>
2023-11-16 22:59:38 +0900

    코드 수정

--- src/main/kotlin/november/flippingEnglishWords.kt
+++ src/main/kotlin/november/flippingEnglishWords.kt
@@ -4,14 +4,15 @@ import java.util.*
 
 fun main(args: Array<String>) = with(Scanner(System.`in`)) {
     val words = next()
-
     val charList: MutableList<Char> = words.toMutableList()
     var lt = 0
     var rt = words.length - 1
 
     while (lt < rt) {
         if (!charList[lt].isLetter()) lt ++
+
         else if (!charList[rt].isLetter()) rt --
+
         else {
             val tmp = charList[lt]
             charList[lt] = charList[rt]
