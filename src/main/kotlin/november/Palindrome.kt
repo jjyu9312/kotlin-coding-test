4b8450a226d83c188e628a883f2749e1f05c5e55
kevin <jjyu9312@gmail.com>
2023-11-29 23:56:55 +0900

    Palindrome

---
+++ src/main/kotlin/november/Palindrome.kt
@@ -0,0 +1,27 @@
+package november
+
+import java.util.*
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    val word = next()
+    val charList = mutableListOf<Char>()
+    word.forEach {
+        if (!charList.contains(it)) {
+            charList.add(it)
+        }
+    }
+
+    val answer = charList.joinToString("")
+    println(answer)
+
+    println("==================================")
+
+    var answer2 = ""
+
+    // indexOf 이용
+    for (i in 0 until word.length) {
+        if (word.indexOf(word[i]) == i) answer2 += word[i]
+    }
+
+    println(answer2)
+}
