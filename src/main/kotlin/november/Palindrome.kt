76e2a9f529c7bef952633587ef0a3bd9f93ad813
kevin <jjyu9312@gmail.com>
2023-11-30 16:04:59 +0900

    회문 문자열 방법 추가

--- src/main/kotlin/november/Palindrome.kt
+++ src/main/kotlin/november/Palindrome.kt
@@ -2,9 +2,18 @@ package november
 
 import java.util.*
 
-fun main(args: Array<String>) = with(Scanner(System.`in`)) {
-    val word = next()
-    val lowerCaseWord = word.toLowerCase()
+fun solution2(str: String): String {
+    val lowerCaseWord = str.toLowerCase()
+    val len = lowerCaseWord.length
+    for (i in 0..len / 2) {
+        if (lowerCaseWord[i] != lowerCaseWord[len - i - 1]) return ("NO")
+    }
+
+    return "YES"
+}
+
+fun solution1(str: String): String {
+    val lowerCaseWord = str.toLowerCase()
     val lowerCaseWordCharList = lowerCaseWord.toCharArray()
     val answerCharList = mutableListOf<Char>()
 
@@ -14,9 +23,15 @@ fun main(args: Array<String>) = with(Scanner(System.`in`)) {
 
     val answer = answerCharList.joinToString("")
 
-    println("word : ${word}")
-    println("answer : ${answer}")
+    println("str : $str")
+    println("answer : $answer")
 
-    if (lowerCaseWord == answer) println("YES")
-    else println("NO")
+    if (lowerCaseWord == answer) return("YES")
+    else return("NO")
+}
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    val str = next()
+    println(solution1(str))
+    println(solution2(str))
 }
