08added68d3f732f3ee5aade1ae767042caf3861
kevin <jjyu9312@gmail.com>
2023-12-08 23:18:23 +0900

    공백 변경

--- src/main/kotlin/november/stringList.kt
+++ src/main/kotlin/november/stringList.kt
@@ -5,10 +5,14 @@ import java.util.*
 fun main(args: Array<String>) = with(Scanner(System.`in`)) {
     val letters = next()
     val word = next()
+
     val charWord = word[0].toLowerCase() // 문자열의 첫 번째 문자를 추출합니다
     val charResult: Char = charWord // Char로 형변환합니다
+
     val lowerCaseSentence = letters.toLowerCase()
+
     var answer = 0
+
     for (item in lowerCaseSentence) {
         if (item == charResult) {
             answer += 1
