fc93a81cb8ad94b2ec16fc558480193478fd58f1
kevin <jjyu9312@gmail.com>
2024-01-08 15:17:37 +0900

    보이는 학생 찾기

---
+++ src/main/kotlin/january/VisibleStudent.kt
@@ -0,0 +1,28 @@
+package january
+
+import java.util.*
+
+fun visibleStudentSolution1(
+    num: Int,
+    intArray: IntArray,
+): Int {
+    var answer = 0
+    val newArray = intArray + 0
+    for (i in 0 until newArray.size - 1) {
+        if (newArray[i] < newArray[i+1]) {
+            answer += 1
+        }
+    }
+
+    return answer
+}
+
+// intArray 만들기
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    val num = nextInt()
+    val intArray = IntArray(num)
+    for (i in 0 until num) {
+        intArray[i] = nextInt() // 배열의 각 요소에 값을 입력받음
+    }
+    println(visibleStudentSolution1(num, intArray))
+}
